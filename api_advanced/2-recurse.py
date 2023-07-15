5741d6a451ee9404332c694920feecc6b2ede30e
k-ganda <116561806+k-ganda@users.noreply.github.com>
2023-07-15 14:44:56 +0200

    Update 2-recurse.py

--- api_advanced/2-recurse.py
+++ api_advanced/2-recurse.py
@@ -17,16 +17,15 @@ def recurse(subreddit, hot_list=[], after=None):
 
     if response.status_code == 200:
         data = response.json().get('data')
-        if data is not None:
-            children = data.get('children')
-            if children is not None:
-                for child in children:
-                    hot_list.append(child.get('data').get('title'))
-                after = data.get('after')
-                if after is not None:
-                    return recurse(subreddit, hot_list, after)
-                else:
-                    return hot_list
+        children = data.get('children') if data else None
+        if children:
+            for child in children:
+                hot_list.append(child.get('data').get('title'))
+            after = data.get('after')
+            if after:
+                return recurse(subreddit, hot_list, after)
+            else:
+                return hot_list
         else:
             return hot_list
     else:
